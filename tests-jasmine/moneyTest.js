1dbeaaef9d24c049791a171f8b7f4ce3f5653655
Samyak Rajgruhi <samyakrajgruhi@gmail.com>
2025-01-13 22:02:20 +0530

    added jasmine test framework and created test suite for "formatCurrency()"

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,13 @@
+import {formatCurrency} from "../scripts/utils/money.js"
+
+describe('Test suite: formatCurrency',()=>{
+    it('Converts cents into dollars',()=>{
+        expect(formatCurrency(2095)).toEqual('20.95');
+    });
+    it('Works with zero',()=>{
+        expect(formatCurrency(0)).toEqual('0.00');
+    });
+    it('Rounds off',()=>{
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    });
+});
