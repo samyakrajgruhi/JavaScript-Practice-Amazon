5e732398c538d01f22abfc614b60b5c0dbf0e918
Samyak Rajgruhi <samyakrajgruhi@gmail.com>
2025-01-17 20:27:33 +0530

    Add BackEnd Practice File

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,12 +1,13 @@
 import { renderProductSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import {cars} from "../data/car.js";
-// import '../data/cart-class.js'
+import './backend-practice.js';
+// import {cars} from "../data/car.js"; //
+// import '../data/cart-class.js' //
 
 renderProductSummary();
 renderPaymentSummary();
 
-cars.forEach((car)=>{
+/* cars.forEach((car)=>{
     car.openTrunk();
     car.displayInfo();
     car.go();
@@ -14,16 +15,4 @@ cars.forEach((car)=>{
     car.go();
     car.brake();
     car.displayInfo();
-})
-
-
-
-
-
-
-
-
-
-
-
-
+}); */
