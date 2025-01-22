7eceac5c63869d5253c4f11781c200c891bb25f8
Samyak Rajgruhi <samyakrajgruhi@gmail.com>
2025-01-22 20:11:11 +0530

    used Promise.all method to implement promises on checkout page

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,15 @@
 export let cart;
 
+export function loadCart(fun){
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load',()=>{ 
+        console.log(xhr.response);
+        fun();
+    });
+    xhr.open('GET','https://supersimplebackend.dev/cart');
+    xhr.send();
+}
+
 loadFromStorage();
 
 function loadFromStorage(){
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,24 +1,51 @@
 import { renderProductSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import './backend-practice.js';
 // import {cars} from "../data/car.js"; 
 // import '../data/cart-class.js' 
+Promise.all([
+    new Promise((resolve)=>{
+        loadProducts(()=>{
+            resolve();
+        });
+    }),
+    new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        });
+    })
+]).then(()=>{
+    renderProductSummary();
+    renderPaymentSummary();
+})
 
+/*
 new Promise((resolve)=>{
     loadProducts(()=>{
         resolve();
     });
 }).then(()=>{
+    return new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        });
+    });
+}).then(()=>{
     renderProductSummary();
     renderPaymentSummary();
 });
+*/
 
 /*
 loadProducts(()=>{
-    renderProductSummary();
-    renderPaymentSummary();
+    loadCart(()=>{
+        renderProductSummary();
+        renderPaymentSummary();    
+    });
 });
+*/
 
 
 
